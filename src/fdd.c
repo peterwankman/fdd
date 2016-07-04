e217501fcf8156270e0ef57905d838a0f2493cb4
Martin Wolters <wolters.mar@gmail.com>
2016-07-04 14:20:03 +0200

    Fick dich Purhur

--- src/fdd.c
+++ src/fdd.c
@@ -13,6 +13,8 @@
 
 #define BREAKUP(n) (n) / 100, (n) % 100
 
+static int verbose = 0;
+
 static void usage(const char *argv) {
 	printf("fdd -- Your personal fast food budget\n");
 	printf("USAGE: %s [-i N] [-s N] -t\n", argv);
@@ -72,7 +74,6 @@ static int read_db(int *monthly_budget, int *spent) {
 
 	if((fp = fopen(DATA_FILE, "r")) == NULL) {
 		fprintf(stderr, "ERROR: Cannot open '%s'.\n", DATA_FILE);
-		fprintf(stderr, "       Maybe you forgot to init a new month.\n");
 		return EXIT_FAILURE;
 	}
 
@@ -95,17 +96,59 @@ static int spend(const int amount) {
 		return ret;
 
 	spent += amount;
+	
+	if(verbose)
+		printf("Spending of %d.%02d " CURRENCY " checked in.", BREAKUP(amount));
+	else
+		printf("OK;");
 
-	printf("Spending of %d.%02d " CURRENCY " checked in. ", BREAKUP(amount));
 
-	if(spent > monthly_budget)
-		printf("You overspent your monthly budget!");
+	if(spent > monthly_budget) {
+		if(verbose) {
+			printf(" You overspent your monthly budget!");
+		} else {
+			printf(" OVER;");
+		}
+	}
 	printf("\n");
 
 	return write_db(monthly_budget, spent);
 }
 
-static int print_budget(int summary) {
+static int print_budget(void) {
+	struct tm *stime;
+	int monthly_budget, daily_budget, spent, budget_today, remaining, next_day;
+	int ret;
+
+	if((ret = read_db(&monthly_budget, &spent)) != EXIT_SUCCESS)
+		return ret;
+
+	stime = local_time();
+	daily_budget = monthly_budget / days_this_month();
+	budget_today = monthly_budget * stime->tm_mday / days_this_month();
+	remaining = budget_today - spent;
+
+	if(remaining >= 0) {
+		printf("%02d.%02d.%04d; %d;\n", 
+			stime->tm_mday, stime->tm_mon + 1, stime->tm_year + 1900,
+			remaining);
+	} else if(spent < monthly_budget) {
+		next_day = days_this_month() - (monthly_budget - spent) / daily_budget;
+		if(spent % daily_budget == 0) next_day++;
+		printf("%02d.%02d.%4d;\n", 
+			next_day, stime->tm_mon + 1, stime->tm_year + 1900);
+	} else {
+		if(stime->tm_mon++ > 11) {
+			stime->tm_mon = 0;
+			stime->tm_year++;
+		}
+		printf("01.%02d.%4d;\n", 
+				stime->tm_mon + 1, stime->tm_year + 1900);
+	}
+
+	return EXIT_SUCCESS;
+}
+static int print_budget_verbose(int summary) {
 	struct tm *stime;
 	int monthly_budget, daily_budget, spent, budget_today, remaining, next_day;
 	int ret;
@@ -137,7 +180,7 @@ static int print_budget(int summary) {
 	if(remaining >= 0) {
 		printf("%d.%02d " CURRENCY "\n\n", BREAKUP(remaining));
 	} else if(spent < monthly_budget) {
-		next_day = spent / daily_budget;
+		next_day = days_this_month() - (monthly_budget - spent) / daily_budget;
 		if(spent % daily_budget == 0) next_day++;
 		printf("again on %02d.%02d.%4d\n\n", 
 				next_day, stime->tm_mon + 1, stime->tm_year + 1900);
@@ -152,11 +195,12 @@ int main(int argc, char **argv) {
 	int opt;
 	int print_summary = 0;
 
-	while((opt = getopt(argc, argv, "hi:s:t")) != -1) {
+	while((opt = getopt(argc, argv, "tvi:s:ht")) != -1) {
 		switch(opt) {
+			case 't': print_summary = 1; break;
+			case 'v': verbose = 1; break;
 			case 'i': init_month(atoi(optarg)); break;
 			case 's': spend(atoi(optarg)); break;
-			case 't': print_summary = 1; break;
 			case 'h':
 			default:
 				usage(argv[0]);
@@ -165,5 +209,8 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	return print_budget(print_summary);
+	if(verbose)
+		return print_budget_verbose(print_summary);
+	else
+		return print_budget();
 }
